c32e849de6c41897519ca0dd0b6b590f6d8b4aed
HumbleNarcissus <maciek.tarach06@gmail.com>
2018-03-24 16:03:02 +0100

    All site view

--- src/Components/AllSites.js
+++ src/Components/AllSites.js
@@ -5,20 +5,56 @@ class AllSites extends Component {
     constructor(props){
         super(props);
         this.state = {
+            isLoading: true,
             data: []
         }
     }
 
+
+    componentDidMount(){
+        this.fetchData()
+    }
+
     fetchData(){
-        axios.get('https://webcutter-api.herokuapp.com/sites')
-            .then(response => console.log(response.data.sites))
+        this.setState({data: [], isLoading: true})
+        fetch('https://webcutter-api.herokuapp.com/sites', {method: 'GET'})
+            .then(response => response.json())
+            .then(json => {
+                const data = json.sites;
+                const temp = [];
+                data.forEach(element => {
+                    temp.push(element);
+                });
+                this.setState({data: temp, isLoading: false});
+            })
             .catch(error => console.log("Error while fetching data"))
     }
     
     render(){
         return(
-             <div>
-                 {this.fetchData()}
+             <div className="container">
+                 <h3>All sites:</h3>
+                 <table className="striped">
+                     <thead>
+                        <tr>
+                            <th>Full Link</th>
+                            <th>Shortcut</th>
+                            <th>Expiry Date</th>
+                            <th>Working</th>
+                        </tr>
+                    </thead>
+
+                    <tbody>
+                    {this.state.data.map( item =>
+                    <tr key={item.full_link.toString()}>
+                        <td>{item.full_link}</td>
+                        <td>{item.short_link}</td>
+                        <td>{item.expiry_date}</td>
+                        <td>{item.working.toString()}</td>
+                    </tr>
+                    )}
+                    </tbody>
+                 </table>
              </div>
         )
     }
