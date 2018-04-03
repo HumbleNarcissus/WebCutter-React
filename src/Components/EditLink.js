62af130a70611d896783b9a8a4994858d1efa22a
HumbleNarcissus <maciek.tarach06@gmail.com>
2018-04-03 20:45:52 +0200

    Editing links

--- src/Components/EditLink.js
+++ src/Components/EditLink.js
@@ -1,10 +1,12 @@
 import React, {Component} from 'react';
+import {Link} from 'react-router-dom';
 
 class EditLink extends Component {
     constructor(props){
         super(props);
         this.state = {
-            exists: false
+            exists: false,
+            query: ''
         }
     }
 
@@ -12,6 +14,20 @@ class EditLink extends Component {
         fetch('https://webcutter-api.herokuapp.com/site/' + this.props.match.params.link)
             .then(res => {if(res.ok) this.setState({exists: true})})
             .catch(err => console.log("Error while fetching data"));
+    }
+
+    postData(){
+        fetch('https://webcutter-api.herokuapp.com/site/' + this.props.match.params.link, {
+            method: 'PUT',
+            headers: {
+                'Accept': 'application/json, text/plain, */*',
+                'Content-type': 'application/json'
+            },
+            body: JSON.stringify({site: this.state.query})
+        })
+        .then(res => res.json())
+        .catch(error => console.error('Error:', error))
+        .then(response => console.log('Success:', response));
     }
 
     render(){
@@ -22,8 +38,18 @@ class EditLink extends Component {
                 <form className="col s12">
                     <div className="row">
                         <div className="input-field col s6">
-                            <input placeholder="Full Link" id="full_link" type="text" className="validate" />
+                        <input type="text" className="form-control" placeholder="Full Link"
+                          value={this.state.query} onChange={ event => {this.setState({query: event.target.value})} }
+                          onKeyPress = {event => {
+                              if (event.key === 'Enter' && this.state.query !== ''){
+                                document.getElementById("btn-post").click();
+                              }
+                          }}
+                        />
                             <label htmlFor="full_link">Full Link</label>
+                            <Link to="/" className="btn" disabled={!this.state.query}
+                                id="btn-post" onClick={()=> this.postData()}
+                            >Submit</Link>
                         </div>
                     </div>
                 </form>
